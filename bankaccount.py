d11d3d0151606b58a299f0fd30aa8052f7489bfb
TejashreyaD <tejashreyad@gmail.com>
2025-09-22 12:40:00 -0400

    bank

--- bankaccount.py
+++ bankaccount.py
@@ -2,20 +2,17 @@
 class BankAccount:
     def __init__(self,balance=0):
         self.balance=balance
-    def deposit(self,amount):
-        if (amount>=0):
-            self.balance+=amount
+    def deposit(self,money):
+        if (money>=0):
+            self.balance+=money
             return self.balance
-    def withdraw(self,amount):
-        if (amount>=0):
-            if (amount<=self.balance):
-                self.balance-=amount
+    def withdraw(self,money):
+        if (money>=0):
+            if (money<=self.balance):
+                self.balance-=money
                 return self.balance
             else:
                 return "Insufficient Funds"
     def get_balance(self):
         return self.balance
-
-
-
 
